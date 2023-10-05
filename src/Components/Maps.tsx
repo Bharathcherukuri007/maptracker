c5c30a704446e056ce47b3d22b31d7c5eb257714
BharathCherukur007 <bharathcherukuri00@gmail.com>
2023-10-05 17:40:19 +0530

    second

--- src/Components/Maps.tsx
+++ src/Components/Maps.tsx
@@ -1,5 +1,6 @@
 import tt from '@tomtom-international/web-sdk-maps'
 import React, { useEffect, useRef, useState } from 'react'
+import * as ttapi from '@tomtom-international/web-sdk-services'
 import "../App.css"
 export default function Maps() {
     const mapElement = React.useRef<HTMLDivElement>(null);
@@ -23,7 +24,7 @@ export default function Maps() {
             key: "ZHTzbVIJuUS5mKdWZibARb8AmUGGEZzs",
             container: mapElement.current ?? "",
             center: [longitude , latitude ],
-            zoom: 15,
+            zoom: 14,
             stylesVisibility: {
               trafficFlow: true,
               trafficIncidents: true
@@ -55,8 +56,58 @@ export default function Maps() {
           marker.setPopup(popup).togglePopup();
           
         };
+        map.on('click', (e) => {
+          console.log(e.lngLat);
+        })
+        let sorted = [
+          
+          {
+            lat: 17.4307564,
+            lng: 78.3708978
+          },{
+            lat: 17.4577589 ,
+            lng: 78.3606787
+          }
+
+        ]
+        const drawRoute = (geoJson, map) => {
+          if (map.getLayer('route')) {
+            map.removeLayer('route')
+            map.removeSource('route')
+          }
+          map.addLayer({
+            id: 'route',
+            type: 'line',
+            source: {
+              type: 'geojson',
+              data: geoJson
+            },
+            paint: {
+              'line-color': '#4a90e2',
+              'line-width': 6
+        
+            }
+          })
+        }
+        const recalculateRoutes = () => {
+          ttapi.services
+              .calculateRoute({
+                key: "ZHTzbVIJuUS5mKdWZibARb8AmUGGEZzs",
+                locations: sorted,
+              })
+              .then((routeData: ttapi.CalculateRouteResponse) => {
+                const geoJson = routeData.toGeoJson()
+               drawRoute(geoJson, map);
+            }).catch((e) => {
+              console.log(e);
+            })
+          
+    
+            
+    };
         addMarker();
         setMap(map);
+        recalculateRoutes();
     }, [longitude, latitude]);
     
 
