33352d01628cb3cc213221705f748b493351114c
BharathCherukur007 <bharathcherukuri00@gmail.com>
2023-10-16 17:24:15 +0530

    final

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 
 
 const config = {
-    API_KEY: process.env.REACT_APP_ENV == "dev" ? "https://localhost:7298/api" : "https://maptrackerdemo.netlify.app/"
+    API_KEY: process.env.REACT_APP_ENV == "dev" ? "https://localhost:7298/api" : "https://aspdotnetdemo.azurewebsites.net/api"
 
 
 };
